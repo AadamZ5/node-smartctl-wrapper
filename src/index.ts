b505a324638fc8aac2baae5f2991a97a03333f07
Aadam Zocolo <azocolo@gmail.com>
2020-08-29 09:40:53 -0500

    Change exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 //index.ts will export the module's main functionality.
 export { SmartCtl, SmartCtlWrapper } from "./smartctl/smartctl";
-export { SmartDevice } from "./smartctl/smart-device";
-export { };
+export { SmartDevice, ISmartDevice } from "./smartctl/smart-device";
+export { SmartTest, ISmartTest, ISmartTestEntry } from "./smartctl/smart_test";
